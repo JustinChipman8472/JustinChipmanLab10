8b906b4539841d8ae0d849caf7f2d3f5e981cfda
JustinChipman8472 <n01598472@humber.ca>
2024-03-28 14:39:04 -0400

    Created 4 Fragments and implemented bottom nav menu

--- app/src/main/java/justin/chipman/n01598472/jc/MainActivity.java
+++ app/src/main/java/justin/chipman/n01598472/jc/MainActivity.java
@@ -1,24 +1,40 @@
 package justin.chipman.n01598472.jc;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+
+            int itemId = item.getItemId();
+            if (itemId == R.id.jusnav_justin) {
+                selectedFragment = new Jus1tinFragment();
+            } else if (itemId == R.id.jusnav_chipman) {
+                selectedFragment = new Chip2manFragment();
+            } else if (itemId == R.id.jusnav_n01598472) {
+                selectedFragment = new N015938472Fragment();
+            } else if (itemId == R.id.jusnav_jc) {
+                selectedFragment = new J4CFragment();
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+
+            return true; // Return true to indicate that you've handled the item selection
         });
+
+        // Set default selection
+        bottomNavigationView.setSelectedItemId(R.id.jusnav_justin);
     }
 }
+
